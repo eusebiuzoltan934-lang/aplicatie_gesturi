e7dfcee23509c11de50145d8028d16f068bb8562
eusebiuzoltan934-lang <eusebiuzoltan934@gmail.com>
2026-03-16 13:38:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-# AICI PUI TOT CODUL TĂU PYTHON
-# Exemplu:
-print("Aplicația mea rulează!")
+from kivy.app import App
+from kivy.uix.button import Button
+
+class AplicatieGesturiApp(App):
+    def build(self):
+        return Button(text='Aplicația funcționează!')
+
+if __name__ == '__main__':
+    AplicatieGesturiApp().run()
